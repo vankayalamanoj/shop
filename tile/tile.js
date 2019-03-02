29d6749f7e700c1774e30838fab3c170ac4c51d4
vankayalamanoj <manoj.vankayala@gmail.com>
2019-03-02 08:11:13 +0530

    first

---
+++ tile/tile.js
@@ -0,0 +1,30 @@
+function myEvent1(){
+    window.location.href="test.html";
+}
+
+function myEvent2(){
+    window.location.href="test.html";
+}
+
+function myEvent3(){
+    window.location.href="test.html";
+}
+
+function myEvent4(){
+    window.location.href="test.html";
+}
+function myEvent5(){
+    window.location.href="test.html";
+}
+
+function myEvent6(){
+    window.location.href="test.html";
+}
+
+function myEvent7(){
+    window.location.href="test.html";
+}
+
+function myEvent8(){
+    window.location.href="test.html";
+}
